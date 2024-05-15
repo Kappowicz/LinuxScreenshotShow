61351078d3aa48df79e50901ba4744586313ddc5
Kappowicz <113104296+Kappowicz@users.noreply.github.com>
2024-05-15 08:54:41 +0200

    Update open_screenshot.sh

--- open_screenshot.sh
+++ open_screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define the path and filename for the screenshot.
-SCREENSHOT_PATH=/home/jacob/Pictures/Screenshots/$(date +%Y%m%d%H%M%S).png
+SCREENSHOT_PATH=/<put your folder here>/$(date +%Y%m%d%H%M%S).png
 
 # Take a screenshot and save it to the defined path.
 gnome-screenshot -a -f "$SCREENSHOT_PATH"
